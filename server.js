27c6cf9b6f73a0992ac7c5bde06be017178f99b6
Jatin Arora <jaarora45@gmaill.com>
2020-03-15 19:02:27 -0400

    Adds code for connecting to VM SSL certs

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require("express");
 var app = express();
 var cors = require("cors");
 var path = require("path");
+var ip = require('ip')
 const admin = require('firebase-admin');
 const apiSecret = require('./config/apiKey.json');
 const apiRouter = require('./src/routing/api');
@@ -10,10 +11,13 @@ const dataService = require('./src/routing/routes');
 const https = require('https');
 const fs = require('fs');
 var cookieParser = require('cookie-parser');
-var HTTP_PORT = process.env.PORT || 8082;
+var HTTP_PORT = process.env.PORT || 3000;
 // VM Options
 const vmHostname = '10.102.112.128';
 const vmPort = 10034;
+const localIp = ip.address();
+
+
 /*************DO NOT TOUCH ************************************ */
 app.use(cors());
 app.use(express.static(__dirname));
@@ -29,8 +33,8 @@ if (process.platform == "win32") {
     cert: fs.readFileSync('./cert.pem'),
     passphrase: "recipebox"
   }, app)
-    .listen(3000, "192.168.0.10", function () {
-      console.log('Example app listening on port 3000! Go to https://localhost:3000/');
+    .listen(HTTP_PORT, localIp, function () {
+      console.log('Example app listening on port ' + HTTP_PORT + '  Go to  https://' + localIp + ':3000/');
       dataService.initialize()
         .then(() => {
           console.log('DATABASE CONNECTED SUCCESSFULLY');
@@ -41,9 +45,9 @@ if (process.platform == "win32") {
 }
 else {
   https.createServer({
-    key: fs.readFileSync('./key.pem'),
-    cert: fs.readFileSync('./cert.pem'),
-    passphrase: "recipebox"
+    key: fs.readFileSync('../cert/prj666-2021.key'),
+    cert: fs.readFileSync('../prj666-2021.crt'),
+    ca: fs.readFileSync('../cert/RapidSSL_RSA_CA_2018.crt')
   }, app)
     .listen(vmPort, vmHostname, function () {
       console.log('Example app listening on port 6759 Go to Recipe Box Website');
